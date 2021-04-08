b985f52b18df78e86ba0af41ff5c7f3e238d2cc7
Akhil Raj <akhil.raj@appsteamtechnologies.com>
2021-04-08 10:32:36 +0530

    Add project files.

---
+++ TestApp1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp1
+{
+    public partial class Form1 : Form
+    {
+        Form2 form2 = new Form2();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            form2.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblLight.BackColor = Color.Red;
+            File.WriteAllText("C:/settings.txt", lblLight.BackColor.ToArgb().ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lblLight.BackColor = Color.Green;
+            File.WriteAllText("C:/settings.txt", lblLight.BackColor.ToArgb().ToString());
+        }
+    }
+}
---
+++ TestApp1/Form2.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace TestApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblLight = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblLight
+            // 
+            this.lblLight.BackColor = System.Drawing.Color.Blue;
+            this.lblLight.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblLight.Location = new System.Drawing.Point(31, 25);
+            this.lblLight.Name = "lblLight";
+            this.lblLight.Size = new System.Drawing.Size(382, 373);
+            this.lblLight.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 422);
+            this.Controls.Add(this.lblLight);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLight;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
